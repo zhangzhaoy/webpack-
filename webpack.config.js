601ac9eda129b90afbd6c8e4a69406e9d735bbce
zhangzhaoy <465983013@qq.com>
2019-08-18 22:35:11 +0800

    webpack学习库190818

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {	
+  entry: './src/main.js',//入口文件
+output: {// 输出文件相关的配置(出口文件)
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'bundle.js'
+}
+};
+
+// 当我们在 控制台，直接输入 webpack 命令执行的时候，webpack 做了以下几步：
+//  1. 首先，webpack 发现，我们并没有通过命令的形式，给它指定入口和出口
+//  2. webpack 就会去 项目的 根目录中，查找一个叫做 `webpack.config.js` 的配置文件
+//  3. 当找到配置文件后，webpack 会去解析执行这个 配置文件，当解析执行完配置文件后，就得到了 配置文件中，导出的配置对象
+//  4. 当 webpack 拿到 配置对象后，就拿到了 配置对象中，指定的 入口  和 出口，然后进行打包构建；
